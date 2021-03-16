d35c3ecd0d9aed6e7c531946543f329d9ded4b1c
matvey <lishay.matvey20001506@gmail.com>
2021-03-16 15:03:16 +0300

    added blinker

---
+++ public/js/scripts.js
@@ -0,0 +1,15 @@
+"use strict";
+
+(function (factory) {
+  typeof define === 'function' && define.amd ? define('scripts', factory) : factory();
+})(function () {
+  'use strict';
+
+  function test() {
+    var val = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'test';
+    console.log(val);
+  }
+
+  test('blalbablaalblab');
+});
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlcyI6WyJzY3JpcHRzLmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlwidXNlIHN0cmljdFwiO1xuXG4oZnVuY3Rpb24gKGZhY3RvcnkpIHtcbiAgdHlwZW9mIGRlZmluZSA9PT0gJ2Z1bmN0aW9uJyAmJiBkZWZpbmUuYW1kID8gZGVmaW5lKCdzY3JpcHRzJywgZmFjdG9yeSkgOiBmYWN0b3J5KCk7XG59KShmdW5jdGlvbiAoKSB7XG4gICd1c2Ugc3RyaWN0JztcblxuICBmdW5jdGlvbiB0ZXN0KCkge1xuICAgIHZhciB2YWwgPSBhcmd1bWVudHMubGVuZ3RoID4gMCAmJiBhcmd1bWVudHNbMF0gIT09IHVuZGVmaW5lZCA/IGFyZ3VtZW50c1swXSA6ICd0ZXN0JztcbiAgICBjb25zb2xlLmxvZyh2YWwpO1xuICB9XG5cbiAgdGVzdCgnYmxhbGJhYmxhYWxibGFiJyk7XG59KTsiXSwiZmlsZSI6InNjcmlwdHMuanMifQ==
---
+++ src/js/components/test.js
@@ -0,0 +1,3 @@
+export default function test(val = 'test') {
+    console.log(val)
+}
