de1be2d10e6b172a99e74b048dad0b087f3ace9f
matvey <lishay.matvey20001506@gmail.com>
2021-03-28 17:14:57 +0300

    added some styles and logic for test form

--- src/js/components/firebaseLogic.js
+++ src/js/components/firebaseLogic.js
@@ -1,17 +1,27 @@
 export default function firebaseLogic() {
-    const form = document.querySelector('.js-form');
-    db.collection('users').get().then(querySnapshot => {
+    const classes = {
+        success: 'is-success'
+    }
+
+   /* const containerQuizForm = document.querySelector('[data-quiz-section]');
+    const quizForm = containerQuizForm.querySelector('[data-quiz-form]');*/
+    const containerRegisterForm = document.querySelector('[data-register-section]');
+    const registerForm = containerRegisterForm.querySelector('[data-register-form]');
+   /* db.collection('users').get().then(querySnapshot => {
         querySnapshot.forEach(doc => {
             console.log(doc.data());
         })
-    })
+    })*/
 
-    form.addEventListener('submit', (e) => {
+
+
+    registerForm.addEventListener('submit', (e) => {
         e.preventDefault();
         db.collection('users').add({
-            Name: form.Name.value,
-            Phone: form.Phone.value,
-            email: form.email.value
+            Name: registerForm.Name.value,
+            Phone: registerForm.Phone.value,
+            email: registerForm.email.value
         })
+        containerRegisterForm.classList.add(classes.success);
     })
 }
